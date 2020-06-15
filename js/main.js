66606fd83f51404ad699fbd1a897eb94986647b3
emanuele5180 <macina5180>
2020-06-15 17:27:10 +0200

    fine con due decimali

--- js/main.js
+++ js/main.js
@@ -4,24 +4,25 @@
 //chiedere l’età del passeggero.
 var quantiAnniHai = parseInt (prompt ( "quanti anni hai?" ));
 
-var costoSenzaSconto = kmDaPercorrere * 0.21; // totale senza sconto
-console.log(costoSenzaSconto);
+var costo = kmDaPercorrere * 0.21; // totale senza sconto
+console.log(costo);
 
 // calcolo il prezzo del viaggio con lo sconto per i monorenni
 var scontoMinorenne;
 if ( quantiAnniHai < 18 ) {
-  scontoMinorenne = (costoSenzaSconto * 20) / 100;
+  scontoMinorenne = (costo * 20) / 100;
+  costo = costo - scontoMinorenne;
+
 }
 
 // calcolo il prezzo del viaggio con lo sconto per gli over 65
+else if (quantiAnniHai > 65 ) {
+  costo = costo - ((costo * 40) / 100);
 
+}
+
+console.log(costo);
 
 // fornisco il prezzo del biglietto
-
-
-//calcolare il prezzo totale del viaggio
-//il prezzo del biglietto è definito in base ai km (0.21 € al km)
-//va applicato uno sconto del 20% per i minorenni
-//va applicato uno sconto del 40% per gli over 65.
-
-document.getElementById('biglietto').innerHTML = 'vuoi percorrere ' + kmDaPercorrere + " all'età di"  + quantiAnniHai + 'dovrai spendere' ;
+document.getElementById('biglietto').innerHTML = 'il prezzo del tuo biglietto è di ' + costo.toFixed(2) + ' ' + 'Euro';
+//'vuoi percorrere ' + kmDaPercorrere + " all'età di"  + quantiAnniHai + 'dovrai spendere' ;
